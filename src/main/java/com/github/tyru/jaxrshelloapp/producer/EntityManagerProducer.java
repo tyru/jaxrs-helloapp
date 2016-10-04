88721cccd10ca2b9bc00186ebee70d6187974fb0
tyru <tyru.exe@gmail.com>
2016-10-05 01:43:05 +0900

    Use enum singleton

--- src/main/java/com/github/tyru/jaxrshelloapp/producer/EntityManagerProducer.java
+++ src/main/java/com/github/tyru/jaxrshelloapp/producer/EntityManagerProducer.java
@@ -2,44 +2,25 @@ package com.github.tyru.jaxrshelloapp.producer;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
-import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 
 @ApplicationScoped
 class EntityManagerProducer {
 
-	@Inject
-	private EMHolder holder;
-
-	/**
-	 * @see https://developer.jboss.org/message/656319#656319
-	 */
 	@Produces
 	public EntityManager getEntityManager() {
-		if (holder.getEM() == null || !holder.getEM().isOpen()) {
-			EntityManager em = Persistence.createEntityManagerFactory("h2-unit").createEntityManager();
-			holder.setEM(em);
-		}
-		return holder.getEM();
+		return EntityManagerSingleton.INSTANCE.get();
 	}
 
-	/**
-	 * @see http://kencharos.hatenablog.com/entry/2015_adv_cal_4_jbatch
-	 */
-	@ApplicationScoped
-	static class EMHolder {
-
-		private ThreadLocal<EntityManager> holder = new ThreadLocal<>();
-
-		public void setEM(EntityManager em) {
-			holder.set(em);
+	private static enum EntityManagerSingleton {
+		INSTANCE;
+		private EntityManager em;
+		private EntityManagerSingleton() {
+			em = Persistence.createEntityManagerFactory("h2-unit").createEntityManager();
 		}
-		public EntityManager getEM() {
-			return holder.get();
-		}
-		public void removeEM() {
-			holder.remove();
+		public EntityManager get() {
+			return em;
 		}
 	}
 }
